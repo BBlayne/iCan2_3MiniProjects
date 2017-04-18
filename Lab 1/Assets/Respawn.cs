239c43d3b430991180b61135a08a212a8848a956
Blayne Bradley <blayne.bradley@gmail.com>
2017-04-18 07:56:17 -0400

    Done lab 1.

---
+++ Lab 1/Assets/Respawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+    GameObject spawn;
+
+	// Use this for initialization
+	private void Start () 
+	{
+        spawn = GameObject.Find("Respawn");
+	}
+	
+	// Update is called once per frame
+	private void Update () 
+	{
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.transform.position = spawn.transform.position;
+        }
+    }
+}
---
+++ Lab 1/Assets/Trollolo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trollolo : MonoBehaviour {
+    float duration = 20;
+    float timer = 0;
+
+	// Use this for initialization
+	private void Start () 
+	{
+        //GameObject[] cubes = GameObject.FindGameObjectsWithTag("Cube");
+        timer = duration;
+
+    }
+	
+	// Update is called once per frame
+	private void Update () 
+	{
+        timer -= Time.deltaTime;
+        print(timer);
+        if (timer <= 0)
+        {
+            GameObject[] cubes = GameObject.FindGameObjectsWithTag("Cube");
+            if (cubes.Length > 5)
+            {
+                foreach (GameObject cube in cubes)
+                {
+                    Destroy(cube);
+                }
+            }
+
+            timer = duration;
+        }
+	}
+}
